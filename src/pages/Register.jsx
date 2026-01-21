039795141062947aa30803136677ee353294dd70
Arvind1805 <varvind1805@gmail.com>
2026-01-21 18:20:00 +0530

    Minor bug fixes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { UserPlus, Mail, Lock, GraduationCap } from "lucide-react";
+// import { UserPlus, Mail, Lock, GraduationCap } from "lucide-react";
 import API from "../api/axios";
 import { useAuth } from "../context/AuthContext";
 import "./Auth.css";
